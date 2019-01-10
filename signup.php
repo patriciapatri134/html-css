5259769ac629a1aae138e5b4ebdd052f937b7dd7
PHP Grupa 1 <phpgrupa1@Admins-iMac-19.local>
2019-01-10 20:41:33 +0200

    10 ianuarie 2019

--- signup.php
+++ signup.php
@@ -12,8 +12,10 @@ $users = get_users();
 
 if(in_array($_POST['email'], $users)) error ("User already exists");
 
-$line = $_POST['email'] . ":" . $_POST['psw'] . PHP_EOL;
+$line = PHP_EOL . $_POST['email'] . ":" . $_POST['psw'] ;
 file_put_contents("user.txt", $line, FILE_APPEND);
+
+echo "Welcome!";
 
 function get_users() {
 	
@@ -22,6 +24,9 @@ function get_users() {
 	$users = [];
 	
 	foreach($lines as $line) {
+		if(trim($line) == '') {
+			continue;
+		}
 		list($email, $psw) = explode(":", trim($line), 2);
 		$users[] = $email;
 	}
